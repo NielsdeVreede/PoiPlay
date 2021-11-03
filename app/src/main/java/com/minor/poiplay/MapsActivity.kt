fec162af29692204adb5c580d199b65f93d0fb13
Niels de Vreede <nielsdevreede@Nielss-MacBook-Pro.local>
2021-11-03 15:07:50 +0100

    added api key for map and added first search bar on top of map

--- app/src/main/java/com/minor/poiplay/MapsActivity.kt
+++ app/src/main/java/com/minor/poiplay/MapsActivity.kt
@@ -22,6 +22,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
