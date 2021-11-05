a65f2201808f7cbf926491dd99d483882eda0dc7
Niels de Vreede <nielsdevreede@Nielss-MacBook-Pro.local>
2021-11-05 15:14:52 +0100

    added popupview

--- app/src/main/java/com/minor/poiplay/MapsActivity.kt
+++ app/src/main/java/com/minor/poiplay/MapsActivity.kt
@@ -2,6 +2,9 @@ package com.minor.poiplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +13,20 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.minor.poiplay.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.Marker
+import android.widget.Toast
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+
+    private lateinit var popUpView: View
+    private lateinit var titleText: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,23 +40,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+        /*Initialize frontend components*/
+        popUpView = findViewById(R.id.popUpView)
+        titleText = findViewById(R.id.titleText)
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+        val eindhoven = LatLng(51.441642, 5.4697225)
+        val defaultMarker = mMap.addMarker(MarkerOptions().position(eindhoven).title("Marker in Eindhoven"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(eindhoven))
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(eindhoven, 25.0f)
+        )
+        mMap.setOnMarkerClickListener( GoogleMap.OnMarkerClickListener {marker ->
+            val markerName = marker.title
+            Toast.makeText(this@MapsActivity, "Clicked location is $markerName", Toast.LENGTH_SHORT)
+                .show()
+            false})
+        }
 }
