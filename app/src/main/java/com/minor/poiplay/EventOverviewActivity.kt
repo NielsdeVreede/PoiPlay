2b90cd3a46ebe4a57cc27dcafcf61599eb2bd12d
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-11-12 12:51:33 +0100

    Add event overview page

---
+++ app/src/main/java/com/minor/poiplay/EventOverviewActivity.kt
@@ -0,0 +1,11 @@
+package com.minor.poiplay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EventOverviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.event_overview_page)
+    }
+}
