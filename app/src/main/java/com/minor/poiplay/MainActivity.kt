9d99dda3d532ce8bcf7e8e270241a4cabc625e8c
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-11-18 13:47:03 +0100

    Toolbar implemented for back navigation

--- app/src/main/java/com/minor/poiplay/MainActivity.kt
+++ app/src/main/java/com/minor/poiplay/MainActivity.kt
@@ -2,10 +2,28 @@ package com.minor.poiplay
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        navigation_toolbar.setNavigationIcon(R.drawable.ic_back_arrow);
+        setSupportActionBar(navigation_toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
