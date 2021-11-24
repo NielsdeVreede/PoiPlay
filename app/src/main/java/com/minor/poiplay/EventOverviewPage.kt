11eafce5edd5a33ef17bac22362a6b8c9c193b8e
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-11-24 13:42:17 +0100

    Page setup

--- app/src/main/java/com/minor/poiplay/EventOverviewPage.kt
+++ app/src/main/java/com/minor/poiplay/EventOverviewPage.kt
@@ -3,6 +3,17 @@ package com.minor.poiplay
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.event_overview_page.*
 
 class EventOverviewPage : Fragment(R.layout.event_overview_page) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        event_title.text = "Groot partijtje "
+        event_time_title.text = "12:30"
+        event_location_title.text = "Voetbalveld, Voskuilen 2A"
+        event_description.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dui felis, imperdiet eu urna ut, malesuada tempus ligula"
+        event_attendant_number.text = "12"
+    }
 }
