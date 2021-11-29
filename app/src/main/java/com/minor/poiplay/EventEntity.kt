ea80dfa40993f7a00a76c1869713f1e417cdc2b7
nielsdevreede <G684b1228>
2021-11-29 11:32:55 +0100

    fixed eventoverview

--- app/src/main/java/com/minor/poiplay/EventEntity.kt
+++ app/src/main/java/com/minor/poiplay/EventEntity.kt
@@ -4,9 +4,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class EventEntity(
-    val id: String,
+    val id: Int,
     val name: String,
     val poi_id: Int,
     val time: String,
-    val attendees: List<AttendantEntity>,
+    val attendees: List<AttendantEntity> = listOf()
 ) : java.io.Serializable
